4198f8c8e4750778a65e7a63bdb072e991d9d348
ARIBILGI <ARIBILGI@PC10402>
2024-12-01 18:08:16 +0300

    Add project files.

---
+++ muhahasebe/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace muhahasebe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection conn = new SqlConnection("Data Source=PC10402\\SQLEXPRESS;Initial Catalog=muhasebe;Integrated Security=True;TrustServerCertificate=True");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            doldur();
+        }
+        private void doldur()
+        {
+            string cumle = "select * from firma";
+            SqlDataAdapter adapter = new SqlDataAdapter(cumle, conn);
+            DataTable tb = new DataTable();
+            adapter.Fill(tb);
+            dataGridView1.DataSource = tb;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            string cumle = "insert into firma (Ad,VergiNo,Adres,Tel) values (@Ad,@VergiNo,@Adres,@Tel)";
+            SqlCommand cmd=new SqlCommand(cumle, conn);
+            cmd.Parameters.Clear();
+            cmd.Parameters.AddWithValue("@Ad",textBox1.Text);
+            cmd.Parameters.AddWithValue("@VergiNo", textBox2.Text);
+            cmd.Parameters.AddWithValue("@Adres", textBox3.Text);
+            cmd.Parameters.AddWithValue("@Tel", textBox4.Text);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Kayıt Eklendi");
+            doldur();
+
+            conn.Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int satir = dataGridView1.CurrentRow.Index;
+            textBox1.Text = dataGridView1.Rows[satir].Cells[1].Value.ToString();
+            textBox2.Text = dataGridView1.Rows[satir].Cells[2].Value.ToString();
+            textBox3.Text = dataGridView1.Rows[satir].Cells[3].Value.ToString();
+            textBox4.Text = dataGridView1.Rows[satir].Cells[4].Value.ToString();
+            textBox5.Text = dataGridView1.Rows[satir].Cells[0].Value.ToString();
+
+        }
+    }
+}
---
+++ muhahasebe/ürünler.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace muhahasebe
+{
+    public partial class ürünler : Form
+    {
+        public ürünler()
+        {
+            InitializeComponent();
+        }
+        SqlConnection conn = new SqlConnection("Data Source=PC10402\\SQLEXPRESS;Initial Catalog=muhasebe;Integrated Security=True;TrustServerCertificate=True");
+        private void ürünler_Load(object sender, EventArgs e)
+        {
+            doldur();
+        }
+
+        private void doldur()
+        {
+            string cumle = "Select * from kategoriler";
+            SqlDataAdapter da = new SqlDataAdapter(cumle,conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                string cumle = "insert into kategoriler (kategoriad) values (@kategoriad)";
+                SqlCommand cmd = new SqlCommand(cumle,conn);
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@kategoriad", textBox1.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("kaydınız eklenmiştir");
+
+                conn.Close();
+                doldur();
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("hata oluştu:"+ex.Message);
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int satir = dataGridView1.CurrentRow.Index;
+            textBox1.Text = dataGridView1.Rows[satir].Cells[1].Value.ToString();
+            textBox2.Text = dataGridView1.Rows[satir].Cells[0].Value.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult cevap = MessageBox.Show("bu kaydı silmek istiyormusunuz","silme", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (cevap == DialogResult.Yes)
+                {
+                    conn.Open();
+                    string cumle = "delete from kategoriler where kategoriId=@Id";
+                    SqlCommand cmd = new SqlCommand(cumle, conn);
+                    cmd.Parameters.Clear();
+                    cmd.Parameters.AddWithValue("@Id", textBox2.Text);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("kaydınız silinmiştir");
+
+                    conn.Close();
+                    doldur();
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("hata oluştu:" + ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                string cumle = "update kategoriler set kategoriad=@kategoriAd where kategoriId=@Id";
+                SqlCommand cmd = new SqlCommand(cumle, conn);
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@kategoriAd", textBox1.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("kaydınız silinmiştir");
+
+                conn.Close();
+                doldur();
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("güncellenmiştir:" + ex.Message);
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox3.Text))
+            {
+                MessageBox.Show("arama kutusu boş olamaz");
+                textBox3.Focus();
+                textBox3.BackColor = Color.BlueViolet;
+            }
+            else
+            {
+                string cumle = "Select * from kategoriler where kategoriad like '%" + textBox3.Text + "%'";
+                SqlDataAdapter da = new SqlDataAdapter(cumle,conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+                if (dt.Rows.Count ==0)
+                {
+                    MessageBox.Show("kayıt bulunmadı");
+                }
+            }
+        }
+    }
+}
